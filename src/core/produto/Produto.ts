7c3bb49ca809976d779f5a36236972cc091ad098
Claudemir Lopes <claudemir.slopes@hotmail.com>
2024-10-01 10:40:36 -0300

    __Projeto_Ecommerce__Gam3rStore

---
+++ src/core/produto/Produto.ts
@@ -0,0 +1,11 @@
+export default interface Produto {
+    id: number
+    nome: string
+    descricao: string
+    marca: string
+    modelo: string
+    imagem: string
+    videoReview: string
+    nota: number
+    tags: string[] 
+}
